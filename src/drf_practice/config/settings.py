4338be57177daef50466e8d28a9086edb4414d10
Satoh_D <sato.dik+github@gmail.com>
2020-10-16 14:53:12 +0900

    【P.150】settings.pyに諸々追加

--- src/drf_practice/config/settings.py
+++ src/drf_practice/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from datetime import timedelta  # (2) DRFのJWT認証の設定
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party apps
+    'rest_framework',   # (1) rest_frameworkの追加
+    'djoser',           # (1) djsoserの追加
+
+    # My Applications
+    'apiv1.apps.Apiv1Config',   # (1) API用アプリケーションの追加
+    'shop.apps.ShopConfig',     # (1) モデル用アプリケーションの追加
 ]
 
 MIDDLEWARE = [
@@ -54,7 +63,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # (3) テンプレートファイルの検索ディレクトリに「<ベースディレクトリ>/templates」を指定
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
@@ -103,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'        # (5) タイムゾーンの変更
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'    # (5) タイムゾーンの変更
 
 USE_I18N = True
 
@@ -118,3 +131,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]   # (4) 静的ファイルの置き場所に「<ベースディレクトリ>/static」を指定
+
+# REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication', # (2) DRFのJWT認証の設定
+    ]
+}
+
+SIMPLE_JWT = {                                      # (2) DRFのJWT認証の設定
+    'AUTH_HEADER_TYPES': ('JWT',),                  # (2) DRFのJWT認証の設定
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30)  # (2) DRFのJWT認証の設定
+}                                                   # (2) DRFのJWT認証の設定
