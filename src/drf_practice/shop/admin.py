db02528b5b9f04afeb81be78ef99acb235ece9ea
Satoh_D <sato.dik+github@gmail.com>
2020-10-16 10:48:51 +0900

    【P.134】管理サイトで登録・編集ができるように設定を追加（リスト9.3）

--- src/drf_practice/shop/admin.py
+++ src/drf_practice/shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+class BookModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'id', 'created_at')   # 固定長リストはtupleで指定するとパフォーマンス○
+    ordering = ('-created_at',)                             # 管理画面での表示順序を指定
+    readonly_fields = ('id', 'created_at')                  # 編集不可項目の指定
+
+
+admin.site.register(Book, BookModelAdmin)
