e4ed79012d9f8376a81a948de113195c9150d613
Satoh_D <sato.dik+github@gmail.com>
2020-10-19 09:38:24 +0900

    【P.157】BootstrapVue, router, storeを使えるように修正（リスト10.11）

--- src/drf_practice/frontend/src/main.js
+++ src/drf_practice/frontend/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
-import App from './App.vue'
+// BootstrapVue
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 実行モードを表す環境変数。Vue CLI 3を使っている場合は
+//   「npm run build」実行時は「production」
+//   「npm run serve」実行時は「development」
+// 自動的にセットされる
+Vue.config.productionTip = process.env.NODE_ENV === 'production'
+
+Vue.use(BootstrapVue)
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
